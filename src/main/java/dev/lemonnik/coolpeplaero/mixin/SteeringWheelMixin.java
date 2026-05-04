b59727c2513d59b12a32c6ade396390f722a3a09
lemonnik <lemongamer6484@gmail.com>
2026-05-04 17:11:38 +0200

    fix steering sound logic

--- src/main/java/dev/lemonnik/coolpeplaero/mixin/SteeringWheelMixin.java
+++ src/main/java/dev/lemonnik/coolpeplaero/mixin/SteeringWheelMixin.java
@@ -6,16 +6,12 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SteeringWheelBlockEntity.class)
 public class SteeringWheelMixin {
-    @Unique
-    private static final float coolPeplAero$EPSILON = 0.5F;
-
     @Shadow
     private float clientAngle;
 
@@ -29,10 +25,7 @@ public class SteeringWheelMixin {
         Level level = self.getLevel();
         if (level == null || !level.isClientSide()) return;
 
-        boolean wasAtZero = Math.abs(oldClientAngle) < coolPeplAero$EPSILON;
-        boolean isAtZero  = Math.abs(clientAngle) < coolPeplAero$EPSILON;
-
-        if (isAtZero && !wasAtZero) {
+        if (clientAngle * oldClientAngle < 0) {
             level.playLocalSound(self.getBlockPos(), SoundEvents.TRIPWIRE_CLICK_OFF, SoundSource.BLOCKS, 0.5F, 0.2F, false);
         }
     }
